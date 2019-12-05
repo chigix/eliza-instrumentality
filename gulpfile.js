34b95bf78c672b02ffcc99edebae4eb45d83ea1c
Richard Lea <chigix@zoho.com>
2019-12-05 11:57:35 +0900

    fix: build script on using parallel computing in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,78 +2,84 @@ const gulp = require('gulp'),
   log = require('fancy-log'),
   fs = require('fs-extra'),
   path = require('path'),
-  del = require('del'),
-  vinylPaths = require('vinyl-paths'),
   ts = require('gulp-typescript');
 
 const PACKAGES_DIR = path.resolve(__dirname, './packages');
-const PACKAGE_TYPES = {
-  NODE: ['eliza-util', 'eliza-core', 'eliza-shell', 'eliza-jp'],
-  ANGULAR: ['playground'],
+const TYPE_TO_DEST = {
+  cjs: 'dist',
+  esm5: 'dist/_esm5',
+  esm2015: 'dist/_esm2015',
 };
 
 /**
- * @param string[] type string[]
+ * @param string type string
  */
-function getPackages(projectFilter) {
+function preparePackageContext(packageName) {
   return fs.readdirSync(PACKAGES_DIR)
     .map(file => path.resolve(PACKAGES_DIR, file))
     .filter(f => fs.lstatSync(path.resolve(f)).isDirectory())
     .map(dir => ({
       dir,
       pkgJson: require(path.resolve(dir, 'package.json')),
-    })).filter(pkg => projectFilter ? projectFilter.indexOf(pkg.pkgJson.name) > -1 : true);
+    })).find(pkg => pkg.pkgJson.name === packageName);
 }
 
-/**
- *
- * Gulp Tasks
- */
-
-gulp.task('ts-compiling', cb => Promise.all(getPackages(PACKAGE_TYPES.NODE).map(pkg => Promise.all(
-  fs.readdirSync(pkg.dir).filter(f => fs.lstatSync(path.resolve(pkg.dir, f)).isFile())
+function getTsContexts(packageContext) {
+  return fs.readdirSync(packageContext.dir)
+    .filter(f => fs.lstatSync(path.resolve(packageContext.dir, f)).isFile())
     .map(f => f.match(/tsconfig\.(cjs|esm5|esm2015|esm5\.rollup|migrations|types)\.json$/))
     .filter(tsconfigMatch => tsconfigMatch && tsconfigMatch.length && tsconfigMatch.length > 1)
-    .map(tsconfig => new Promise(resolve => {
-      const tsconfigFile = path.resolve(pkg.dir, tsconfig[0]);
-      log(`ts-compiling: ${tsconfigFile}`);
-      const tsProject = ts.createProject(tsconfigFile);
-      const tsReplaceProject = ts.createProject(tsconfigFile);
-      const tsResult = gulp.src(['src/**/*.ts', '!**/*.spec.ts'], { cwd: pkg.dir })
-        .pipe(tsProject());
-      const tsReplace = gulp.src(['src-replace/**/*.ts'], { cwd: pkg.dir })
-        .pipe(tsReplaceProject());
-      if (tsconfig[1] === 'types') {
-        tsResult.dts.pipe(gulp.dest('dist', { cwd: pkg.dir }));
-      } else {
-        const dest = {
-          cjs: 'dist',
-          esm5: 'dist/_esm5',
-          esm2015: 'dist/_esm2015',
-        }[tsconfig[1]];
-        tsResult.js.pipe(gulp.dest(dest, { cwd: pkg.dir }));
-        gulp.src(['src/**/*', '!src/**/*.ts',
-          '!src/**/__snapshots__', '!src/**/__snapshots__/**/*'], {
-          base: path.resolve(pkg.dir, 'src'),
-          cwd: pkg.dir,
-        }).pipe(gulp.dest(dest, { cwd: pkg.dir }));
-        tsReplace.js
-          .pipe(vinylPaths(async builtFile => {
-            await del(
-              path.resolve(pkg.dir, 'dist',
-                path.relative(pkg.dir + '/src-replace', builtFile)));
-          }))
-          .pipe(gulp.dest(dest, { cwd: pkg.dir, overwrite: true }));
-      }
-      resolve();
-    }))
-))).then(() => cb()));
+    .map(tsconfig => ({
+      dir: packageContext.dir,
+      pkgJson: packageContext.pkgJson,
+      tsconfigFile: path.resolve(packageContext.dir, tsconfig[0]),
+      buildType: tsconfig[1],
+    }));
+}
 
-gulp.task('make-packages', cb => Promise.all(getPackages(PACKAGE_TYPES.NODE).map(
-  pkg => new Promise(resolve => {
-    const distPath = path.resolve(pkg.dir, 'dist');
+function compileTs(tsContext) {
+  function compileTsTask() {
+    const tsProject = ts.createProject(tsContext.tsconfigFile);
+    const tsResult = gulp.src(['src/**/*.ts', '!**/*.spec.ts'], { cwd: tsContext.dir })
+      .pipe(tsProject());
+    if (tsContext.buildType === 'types') {
+      return tsResult.dts.pipe(gulp.dest('dist', { cwd: tsContext.dir }));
+    } else {
+      const dest = TYPE_TO_DEST[tsContext.buildType];
+      return tsResult.js
+        .pipe(gulp.src(
+          ['src/**/*', '!src/**/*.ts',
+            '!src/**/__snapshots__', '!src/**/__snapshots__/**/*'], {
+          base: path.resolve(tsContext.dir, 'src'),
+          cwd: tsContext.dir,
+        })).pipe(gulp.dest(dest, { cwd: tsContext.dir }));
+    }
+  };
+  compileTsTask.displayName = `${tsContext.pkgJson.name} - ${tsContext.buildType}`;
+  return compileTsTask;
+}
+
+function compileTsReplace(tsContext) {
+  function compileTsTask() {
+    const tsProject = ts.createProject(tsContext.tsconfigFile);
+    const tsResult = gulp.src(['src-replace/**/*.ts'], { cwd: tsContext.dir })
+      .pipe(tsProject());
+    if (tsContext.buildType === 'types') {
+      return tsResult.dts.pipe(gulp.dest('dist', { cwd: tsContext.dir }));
+    } else {
+      const dest = TYPE_TO_DEST[tsContext.buildType];
+      return tsResult.js.pipe(gulp.dest(dest, { cwd: tsContext.dir }));
+    }
+  };
+  compileTsTask.displayName = `${tsContext.pkgJson.name}[src-replace] - ${tsContext.buildType}`;
+  return compileTsTask;
+}
+
+function assemblePackageFiles(packageContext) {
+  function assemblePackageTask() {
+    const distPath = path.resolve(packageContext.dir, 'dist');
     const rootPackageJson = Object.assign({},
-      require(path.resolve(pkg.dir, 'package.json')),
+      require(path.resolve(packageContext.dir, 'package.json')),
       {
         main: './index.js',
         typings: './index.d.ts',
@@ -85,11 +91,51 @@ gulp.task('make-packages', cb => Promise.all(getPackages(PACKAGE_TYPES.NODE).map
       rootPackageJson.es2015 = './_esm2015/index.js';
     }
     delete rootPackageJson.scripts;
-    if (fs.existsSync(path.resolve(pkg.dir, 'README.md'))) {
-      fs.copyFileSync(path.resolve(pkg.dir, 'README.md'), `${distPath}/README.md`);
+    if (fs.existsSync(path.resolve(packageContext.dir, 'README.md'))) {
+      fs.copyFileSync(path.resolve(packageContext.dir, 'README.md'), `${distPath}/README.md`);
     }
-    resolve({ dest: `${distPath}/package.json`, rootPackageJson });
     log('make-packages: writing: ' + `${distPath}/package.json`);
-  }).then(
-    ({ dest, rootPackageJson }) => fs.writeJson(dest, rootPackageJson, { spaces: 2 })
-  ))).then(() => cb()));
+    return fs.writeJson(`${distPath}/package.json`, rootPackageJson, { spaces: 2 });
+  }
+  assemblePackageTask.displayName = `${packageContext.pkgJson.name} - assemble`;
+  return assemblePackageTask;
+}
+
+exports.buildPackages = gulp.parallel(
+  gulp.series(
+    gulp.parallel(
+      getTsContexts(preparePackageContext('eliza-util')).map(compileTs),
+    ),
+    gulp.parallel(
+      getTsContexts(preparePackageContext('eliza-util')).map(compileTsReplace),
+      assemblePackageFiles(preparePackageContext('eliza-util')),
+    ),
+  ),
+  gulp.series(
+    gulp.parallel(
+      getTsContexts(preparePackageContext('eliza-core')).map(compileTs),
+    ),
+    gulp.parallel(
+      getTsContexts(preparePackageContext('eliza-core')).map(compileTsReplace),
+      assemblePackageFiles(preparePackageContext('eliza-core')),
+    ),
+  ),
+  gulp.series(
+    gulp.parallel(
+      getTsContexts(preparePackageContext('eliza-shell')).map(compileTs),
+    ),
+    gulp.parallel(
+      getTsContexts(preparePackageContext('eliza-shell')).map(compileTsReplace),
+      assemblePackageFiles(preparePackageContext('eliza-shell')),
+    ),
+  ),
+  gulp.series(
+    gulp.parallel(
+      getTsContexts(preparePackageContext('eliza-jp')).map(compileTs),
+    ),
+    gulp.parallel(
+      getTsContexts(preparePackageContext('eliza-jp')).map(compileTsReplace),
+      assemblePackageFiles(preparePackageContext('eliza-jp')),
+    ),
+  ),
+);
